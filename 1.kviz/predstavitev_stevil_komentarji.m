ffb63d61be57afd7a51d7857df27185c0d192bbc
JanKolenc <ostrostrelec.jan@gmail.com>
2018-12-04 18:37:14 +0100

    Update predstavitev_stevil_komentarji.m

--- 1.kviz/predstavitev_stevil_komentarji.m
+++ 1.kviz/predstavitev_stevil_komentarji.m
@@ -1,5 +1,11 @@
 % sistem
 b = 5; t = 4; L = -5; U = 5;
+
+% b = baza , t= dolzina mantise, L,U=meje eksponenta
+%kodo rabim povecat za for zanko ce imam vecji t oz zmanjsati
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % mantise
 c = 0:b -1;
 M = zeros ( b ^t ,1) ;
@@ -14,7 +20,10 @@ for c1 = c
         end
     end
 end
-% normalizirana števila
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% normalizirana ï¿½tevila
 d = U - L +1;
 bm = b ^( t -1) ;
 Xpn = zeros (( b -1) * bm , d ) ;
@@ -24,52 +33,94 @@ end
 Xpn = Xpn (:) ;
 Xn = [- Xpn( end : -1:1) ; Xpn ];
 
-% denormalizirana števila
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% denormalizirana stevila
 Xpd = M (2: b ^( t -1) ) * b ^ L;
 Xd = [- Xpd(end: -1:1) ; Xpd ];
 
-% predstavljiva števila ( brez 0 , Inf , -Inf in NaN)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% predstavljiva stevila ( brez 0 , Inf , -Inf in NaN)
 X = [ Xn(1: end /2) ; Xd(1: end /2) ; Xpd ; Xpn ];
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %delez denormaliziranih stevil
-delezXd = length(Xd) / length(X)
+delez_denormaliziranih = length(Xd) / length(X)
 
-%stevilo normaliziranih stevil, manjsih od ()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%delez normaliziranih stevil
+delez_normaliziranih = length(Xn) / length(X)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%stevilo normaliziranih stevil, manjsih od (pi)
+
+stevilo = input ('Vnesite stevilo : ') ; 
 stevec1 = 0;
 for i = (1: length(Xn));
-    if Xn(i) < pi
-        stevec1 = stevec1 +1
+    if Xn(i) < stevilo
+        stevec1 = stevec1 +1;
     end
 end
+fprintf('Stevilo normaliziranih stevil, manjsih od %d je %d.\n',stevilo,stevec1)
 
-st_pi = [];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+razlika = input ('Vnesite manj od koliko naj se razlikujejo: ') ;
+najvecje = [];
 for i = (1 : length (X));
-    if abs(X(i) - pi) < 1;
-        st_pi = [st_pi; X(i)];
+    if abs(X(i) - stevilo) < razlika;
+        najvecje = [najvecje; X(i)];
     end
 end
 
 razmiki = [];
-for i = [2: length(st_pi)];
-    razmiki = [razmiki; (st_pi(i) - st_pi(i-1))];
+for i = [2: length(najvecje)];
+    razmiki = [razmiki; (najvecje(i) - najvecje(i-1))];
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 povprecni_razmiki = sum(razmiki) / length(razmiki)
 
 
-stevilo = 0;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+stevilo1 = 0;
 razdalja = 1000;
 for i = [1: length(X)];
     st = X(i);
-    d = abs(pi - st);
+    d = abs(stevilo - st);
     if d < razdalja
         razdalja = d;
-        stevilo = st;
+        stevilo1 = st;
     end
 end
-nablizje_stevilo = stevilo
+nablizje_stevilo = stevilo1;
+fprintf('Predstavljivo stevilo, ki najblizje stevilu %d je %d (oz FL(X)).\n',stevilo,nablizje_stevilo)
+
         
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+najvecje = [];
+for i = (1 : length (X));
+    if X(i) < stevilo;
+        najvecje = [najvecje; X(i)];
+    end
+end
+najvecje_st = max(najvecje);
+fprintf('Najvecje stevilo iz sistema, ki je manjse od %d je %d.\n',stevilo,najvecje_st)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+najmanjse = [];
+for i = (1 : length (X));
+    if X(i) > stevilo;
+        najmanjse = [najmanjse; X(i)];
+    end
+end
+najmanjse_st = min(najmanjse);
+fprintf('Najmanjse stevilo iz sistema, ki je vecje od %d je %d.\n',stevilo,najmanjse_st)
