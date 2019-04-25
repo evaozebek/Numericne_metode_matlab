6bbb9702eff3bdb45cb9efb080a844254ed365cc
Eva <eva.ozebek@gmail.com>
2019-04-25 16:23:46 +0200

    dodano normiranje začetnega vektorja

--- 2. semester/DN_1/inverzna_iteracija.m
+++ 2. semester/DN_1/inverzna_iteracija.m
@@ -1,11 +1,13 @@
 function [z] = inverzna_iteracija(A,z,st_korakov,zac_prbl, tol)
 %A... matrika
-%z... zaèetni vektor
+%z... zaÃ¨etni vektor
 %tol... toleranca
 %zac_prbl... zacetni priblizek za lastno vrednost
 
 A = A - zac_prbl*eye(size(A));
+z = z/norm(z,2)
 k=1;
+
 while k<st_korakov && norm(A*z - zac_prbl*z,2) > tol
     y=A\z;
     z=y/norm(y,2);
