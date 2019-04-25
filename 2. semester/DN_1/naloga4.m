20721f79008557f94f54feca9ec17f36c58c563b
Eva <eva.ozebek@gmail.com>
2019-04-25 17:36:22 +0200

    komentarji
    kramar je najboljša oseba na svetu

--- 2. semester/DN_1/naloga4.m
+++ 2. semester/DN_1/naloga4.m
@@ -1,16 +1,18 @@
-G = @(x) exp(-x + sin(10*x));
-dG = @(x) exp(-x + sin(10*x))*(-1 + 10*cos(10*x));
-X = zeros(7,1);
+G = @(x) exp(-x + sin(10*x)); %funkcija
+
+dG = @(x) exp(-x + sin(10*x))*(-1 + 10*cos(10*x)); %odvod funkcije
+
+X = zeros(7,1); %%%vektor x
 for i = 0:6
     X(i+1)= i/6;
 end
 
- dX = zeros(7,1);
+ dX = zeros(7,1); %%%%vektor vrednosti odvoda funkcije v točkah x
  for i = 1:7
      dX(i) = dG(X(i));
  end
  
- Y = zeros(7,1);
+ Y = zeros(7,1); %%%vrednost funkcije v točkah x
  for i = 1:7
      Y(i) = G(X(i));
  end
@@ -20,9 +22,9 @@ for i = 1:6
     test(i) = ((X(i) + X(i+1))/2);
 end
 
-x = linspace(0,1);
-p = pchip(X, Y);
-pp = Hermit(X, Y, dX);
+x = linspace(0,1); %interval x-ov, ki jih rabiš za risanje grafa
+p = pchip(X, Y); % če nimaš odvodov
+pp = Hermit(X, Y, dX); % če imaš odovode
 
 
 
